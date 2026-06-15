f3e7af4b3fb8fba0a2bedb9da8916b2e58c7c6fa
lokeshdudala <lokeshdudala996@gmail.com>
2026-06-15 20:30:51 +0530

    Create UniqueBitWise.java

---
+++ UniqueBitWise.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      int a[]=new int[n];
+      for(int i=0;i<n;i++){
+          a[i]=sc.nextInt();
+      }
+      int sum=0;
+      for(int i=0;i<n;i++){
+          sum=sum^ a[i];
+      }
+      System.out.println("Unique element"+sum);
+    }
+}
