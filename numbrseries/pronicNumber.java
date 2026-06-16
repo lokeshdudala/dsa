5c77614b2cca0e12650671ea8d994d2a85420470
lokeshdudala <lokeshdudala996@gmail.com>
2026-06-16 14:01:01 +0530

    Create pronicNumber.java

---
+++ numbrseries/pronicNumber.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class Main{
+    public static void main(String args[]){
+    int n=10;
+    for(int i=1;i<n;i++){
+        if(i*(i-1)==n){
+            System.out.println("pronic"+" "+i+" "+ (i-1));
+            break;
+    
+    }
+        }
+        System.out.println("nota pronic");
+        
+    }
+}
